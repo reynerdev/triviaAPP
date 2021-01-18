944fd055d1203f37085d40aaa777329cd6410646
Reyner <rloza@pucp.pe>
2021-01-18 16:12:28 -0500

    second part develop

--- src/bullet.js
+++ src/bullet.js
@@ -1,26 +1,47 @@
+
+
 function bullet() {
   console.log('InsideBullet');
   let bulletContainer = document.getElementById('questionCounting');
   let canvasElement = document.createElement('canvas');
   let canvas = canvasElement.getContext('2d');
 
-  canvas.width = 65;
-  canvas.height = 65;
-  canvas.beginPath();
+
+  canvasElement.width= 85
+  canvasElement.height= 65
+  let w  = 65
+let h = canvasElement.height
+console.log(canvasElement.width,h,"Sizes")
+  
+  // canvas.width = "65px";
+  // // canvas.height = "65px";
+  // canvas.beginPath();
   //   canvas.moveTo(canvas.width/2,canvas.height/2)
+  
+
+  canvas.beginPath()
   canvas.arc(
-    canvas.width / 2,
-    canvas.height / 2,
-    canvas.width / 2,
+    w / 2,
+    h / 2,
+    w / 2 -2,
     0 * Math.PI,
     2 * Math.PI,
     false
   );
+  canvas.lineWidth = 3;
+  canvas.stroke();
+    canvas.closePath()
+  canvas.beginPath
+  canvas.lineTo(w,h/2)
+  canvas.lineTo(w+20,h/2)
 
-  canvas.lineWidth = 10;
-  canvas.strokeStyle = 'black';
+  canvas.lineWidth = 3;
+
   canvas.stroke();
 
+  // canvas.fill()
+
+  console.log(canvasElement,"canvas Element")
   bulletContainer.appendChild(canvasElement);
 }
 
