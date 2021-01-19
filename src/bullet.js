c17c2decb21ff140bd875961b3bc482e8b7b4aad
reynerdev <rloza@pucp.pe>
2021-01-19 01:35:48 -0500

    Ultima parte , etructura juego

--- src/bullet.js
+++ src/bullet.js
@@ -1,47 +1,57 @@
-
-
 function bullet() {
   console.log('InsideBullet');
   let bulletContainer = document.getElementById('questionCounting');
   let canvasElement = document.createElement('canvas');
   let canvas = canvasElement.getContext('2d');
 
+  canvasElement.width = 85;
+  canvasElement.height = 65;
+  canvasElement.innerText = '1';
+  let w = 65;
+  let w2 = 45;
+  let h = canvasElement.height;
 
-  canvasElement.width= 85
-  canvasElement.height= 65
-  let w  = 65
-let h = canvasElement.height
-console.log(canvasElement.width,h,"Sizes")
-  
-  // canvas.width = "65px";
-  // // canvas.height = "65px";
-  // canvas.beginPath();
-  //   canvas.moveTo(canvas.width/2,canvas.height/2)
-  
+  // add Text to the canvas
 
-  canvas.beginPath()
-  canvas.arc(
-    w / 2,
-    h / 2,
-    w / 2 -2,
-    0 * Math.PI,
-    2 * Math.PI,
-    false
-  );
-  canvas.lineWidth = 3;
-  canvas.stroke();
-    canvas.closePath()
-  canvas.beginPath
-  canvas.lineTo(w,h/2)
-  canvas.lineTo(w+20,h/2)
-
-  canvas.lineWidth = 3;
-
+  /// drawing the countur of the circle
+  canvas.beginPath();
+  canvas.arc(w / 2, h / 2, w / 2 - 2, 0 * Math.PI, 2 * Math.PI, false);
+  canvas.lineWidth = 2;
+  canvas.strokeStyle = '#E53170';
   canvas.stroke();
 
-  // canvas.fill()
+  canvas.closePath();
 
-  console.log(canvasElement,"canvas Element")
+  // drawing the inner circle fill
+
+  canvas.beginPath();
+  canvas.arc(w / 2, h / 2, w2 / 2 - 2, 0 * Math.PI, 2 * Math.PI, false);
+  canvas.fillStyle = '#E53170';
+  canvas.fill();
+  canvas.closePath();
+
+  // Line to the right of the circle
+
+  let lastElement = false;
+  if (lastElement !== true) {
+    canvas.beginPath;
+    canvas.moveTo(w, h / 2);
+    canvas.lineTo(w + 20, h / 2);
+
+    canvas.lineWidth = 2;
+
+    canvas.stroke();
+
+    canvas.closePath;
+  }
+
+  canvas.beginPath();
+  canvas.fillStyle = 'white';
+  canvas.textAlign = 'center';
+  canvas.font = 'bold 30px Arial';
+  canvas.fillText('1', w / 2, h / 2 + 10);
+  canvas.closePath();
+  console.log(canvasElement, 'canvas Element');
   bulletContainer.appendChild(canvasElement);
 }
 
--- src/retrieveQuestions.js
+++ src/retrieveQuestions.js
@@ -2,14 +2,37 @@ import TOKEN from './utils'
 
 
 
-const retrieveQuestion =  (category,dificulty,type) {
+const retrieveQuestion =  () {
+
+    class manejadorPreguntas {
+
+        constructor(){
+
+            
+        }
+
+
+    }
+
 
     const TOKEN = TOKEN ;
-
+    const questionArray
     // Type: multiple // boolean
     // category: is the ID
 
-    const URI = `https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${dificult}&type=${type}`
+    // const URI = `https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${dificult}&type=${type}`
+    const URI = 'https://opentdb.com/api.php?amount=10'
 
+    fetch(URI).then(Response=>response.json()).then(questions=>{
+
+        questionArray=questions
+
+        //pongo el contener de block a none
+
+
+
+
+
+    })
     
 } 
